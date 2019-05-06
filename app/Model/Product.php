406ba1b7a176b32fa00876b1d105d5a084964b26
achamwada <alexanderchamwada@gmail.com>
2019-05-06 18:28:49 +0100

    Adding relationship between Products and Reviews

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -3,8 +3,12 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Model\Review;
 
 class Product extends Model
 {
-    //
+    // Relationship with the Review Model
+    public function reviews(){
+        return $this->hasMany(Review::class);
+    }
 }
--- app/Model/Review.php
+++ app/Model/Review.php
@@ -3,8 +3,13 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Model\Product;
 
 class Review extends Model
 {
-    //
+    // Relationship with the Product Model
+    public function product(){
+
+        return $this->belongsTo(Product::class);
+    }
 }
